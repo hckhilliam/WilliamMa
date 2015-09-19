8dea4cf787b3c88171bbf54be984a31064f8d875
William Ma <williamjma@hotmail.com>
2015-09-19 19:20:10 -0400

    new domain. And added a bit of difficulty to items. Also refactored shades and items javascript code

--- scripts/items.js
+++ scripts/items.js
@@ -1,68 +1,33 @@
-var score = 0, timer = 0, standard = true, items = 0;
-var inc = 0; //increments for timer
-var maxNumItems = 10;	//current difficulty
-var countdown; //timer
-const NUM_BLOCKS = 25, MAX_SIZE = 20, ROW_SIZE = 5;
+function ItemsGame() {
+    var score = 0, timer = 0, standard = true, pro = false, items = 0;
+    var inc = 0; //increments for difficulty
+    var maxNumItems = 5;	//current difficulty
+    var countdown; //timer
+    const NUM_BLOCKS = 25, MAX_SIZE = 20, ROW_SIZE = 5;
 
-$(function () {
-    $('#standard').click(function() {
-        standard = true;
-        startNewGame();
-    });
+    this.setStandard = function(isStandard) {
+        standard = isStandard;
+    };
 
-    $('#timed').click(function() {
-        standard = false;
-        startNewGame();
-    });
+    this.startNewGame = function(isPro) {
+        if (isPro != undefined) pro = isPro;
+        if (standard) timer = 3;
+        else timer = 60;
 
-    $('#enter').click(answer);
-
-    $('#answer').keypress(function(e) {
-       if (e.which == 13) {
-           answer();
-       }
-    });
-
-
-    $('#highScores').click(function() {
         $('#instructions').hide();
-        var standardScore = localStorage.ItemsStandardHighScore;
-        if (standardScore) $('#standardScore').text('Standard: ' + standardScore);
-        else $('#standardScore').text('Standard: ' + 0);
-        var timedScore = localStorage.ItemsTimedHighScore;
-        if (timedScore) $('#timedScore').text('Timed Mode: ' + timedScore);
-        else $('#timedScore').text('Timed Mode: ' + 0);
-        $('#scores').show();
-    });
-
-    $('#playAgain').click(function() {
+        $('#gametype').hide();
         $('#conclusion').hide();
-        startNewGame();
-    });
+        $('#game').show();
+        randomize();
+        $('#score').text('Score: ' + score);
+        $('#countdown').text('Time Left: ' + timer);
 
-    $('#menuBtn').click(function() {
-        $('#conclusion').hide();
-        $('#instructions').show();
-    });
+        countdown = setInterval(subtractTime, 1000);
 
-    $('#return').click(function() {
-        $('#scores').hide();
-        $('#instructions').show();
-    });
-});
+        $('#answer').focus();
+    };
 
-function startNewGame() {
-    if (standard) timer = 3;
-    else timer = 60;
-
-    $('#instructions').hide();
-    $('#conclusion').hide();
-    $('#game').show();
-    randomize();
-    $('#score').text('Score: ' + score);
-    $('#countdown').text('Time Left: ' + timer);
-
-    countdown = setInterval(function () {
+    function subtractTime() {
         if (--timer > 0) {
             $('#countdown').text('Time Left: ' + timer);
         } else {
@@ -87,47 +52,120 @@ function startNewGame() {
             $('#conclusion').show();
             resetGame();
         }
-    }, 1000);
-
-    $('#answer').focus();
-}
-
-function randomize() {
-    var content = $('#blocks');
-    content.empty();
-    items = Math.ceil(Math.random()*maxNumItems);
-    var listOfPlacedItems = [];
-
-    for (var i=0; i<items; i++) {
-        var position;
-        do { position = Math.floor(Math.random()*NUM_BLOCKS); } while (listOfPlacedItems.indexOf(position) >= 0);
-        listOfPlacedItems.push(position);
-        var left = (position%ROW_SIZE)*MAX_SIZE;
-        var top = Math.floor(position/ROW_SIZE)*MAX_SIZE;
-        content.append('<div id=' + i + ' style="left: ' + left + '%; top: ' + top + '%; width: ' + MAX_SIZE*0.9 + '%; height: ' + MAX_SIZE*0.9 + '%;"></div>');
     }
-}
 
-function resetGame() {
-    score = 0;
-    $('#answer').val('');
-}
+    function randomize() {
+        var content = $('#blocks');
+        content.empty();
+        items = Math.ceil(Math.random()*maxNumItems);
+        var listOfPlacedItems = [];
 
-function answer() {
-    var answer = parseInt($('#answer').val());
-    $('#answer').val('');
-    if (answer == items) {
-        score += 10;
-        $('#score').text('Score: ' + score);
-
-        if (standard) {
-            timer = 3;
-            $('#countdown').text('Time Left: ' + timer);
+        for (var i=0; i<items; i++) {
+            var position;
+            do { position = Math.floor(Math.random()*NUM_BLOCKS); } while (listOfPlacedItems.indexOf(position) >= 0);
+            listOfPlacedItems.push(position);
+            var left = (position%ROW_SIZE)*MAX_SIZE;
+            var top = Math.floor(position/ROW_SIZE)*MAX_SIZE;
+            var randomSize = 18;
+            if (pro) {
+                randomSize = Math.floor(Math.random()*14)+5;
+                //TODO
+            }
+            content.append('<div id=' + i + ' style="left: ' + left + '%; top: ' + top + '%; width: ' + MAX_SIZE*0.25 + '%; height: ' + MAX_SIZE*0.25 + '%;"></div>');
         }
-
-        randomize();
-    } else {
-        score -= 5;
-        $('#score').text('Score: ' + score);
     }
-}
+
+    function resetGame() {
+        score = 0;
+        inc = 0;
+        maxNumItems = 5;
+        $('#answer').val('');
+    }
+
+    this.answer = function() {
+        var answer = parseInt($('#answer').val());
+        $('#answer').val('');
+        if (answer == items) {
+            inc++;
+            if (inc%3 == 0 && maxNumItems < 15) maxNumItems++;
+
+            score += 10;
+            $('#score').text('Score: ' + score);
+
+            if (standard) {
+                clearInterval(countdown)
+                timer = 3;
+                countdown = setInterval(subtractTime, 1000);
+                $('#countdown').text('Time Left: ' + timer);
+            }
+
+            randomize();
+        } else {
+            score -= 5;
+            $('#score').text('Score: ' + score);
+        }
+    };
+
+    this.endGame = function() {
+        resetGame();
+        clearInterval(countdown);
+    };
+};
+
+$(function () {
+    game = new ItemsGame();
+
+    $('#standard').click(function() {
+        $('#instructions').hide();
+        $('#gametype').show();
+        game.setStandard(true);
+    });
+
+    $('#timed').click(function() {
+        $('#instructions').hide();
+        $('#gametype').show();
+        game.setStandard(false);
+    });
+
+    $('#normal').click(function() {
+        game.startNewGame(false);
+    });
+
+    $('#pro').click(function() {
+        game.startNewGame(true);
+    });
+
+    $('#enter').click(game.answer);
+
+    $('#answer').keypress(function(e) {
+        if (e.which == 13) {
+            game.answer();
+        }
+    });
+
+    $('#highScores').click(function() {
+        $('#instructions').hide();
+        var standardScore = localStorage.ItemsStandardHighScore;
+        if (standardScore) $('#standardScore').text('Standard: ' + standardScore);
+        else $('#standardScore').text('Standard: ' + 0);
+        var timedScore = localStorage.ItemsTimedHighScore;
+        if (timedScore) $('#timedScore').text('Timed Mode: ' + timedScore);
+        else $('#timedScore').text('Timed Mode: ' + 0);
+        $('#scores').show();
+    });
+
+    $('#playAgain').click(function() {
+        $('#conclusion').hide();
+        game.startNewGame();
+    });
+
+    $('#menuBtn').click(function() {
+        $('#conclusion').hide();
+        $('#instructions').show();
+    });
+
+    $('#return').click(function() {
+        $('#scores').hide();
+        $('#instructions').show();
+    });
+});
--- scripts/shades.js
+++ scripts/shades.js
@@ -1,20 +1,138 @@
-var score = 0, timer = 0, initSize = 50, threshold = 50, standard = true;
-var odd = 0; //index of block with lighter shade
-var inc = 0; //increments for timer
-var levelCounter = 0;	//current level
-var thresCount = 0, thresInc = 1, thresModder = 0;	//helpers for difficulty increase by lowering the threshold between a normal block and a lighter block
-var sizeCount = 0, sizeInc = 1, size = 2;	//helpers for difficulty increase by increasing # of squares
-var countdown; //timer
+function ShadesGame() {
+	var score = 0, timer = 0, initSize = 50, threshold = 50, standard = true;
+	var odd = 0; //index of block with lighter shade
+	var inc = 0; //increments for timer
+	var levelCounter = 0;	//current level
+	var thresCount = 0, thresInc = 1, thresModder = 0;	//helpers for difficulty increase by lowering the threshold between a normal block and a lighter block
+	var sizeCount = 0, sizeInc = 1, size = 2;	//helpers for difficulty increase by increasing # of squares
+	var countdown; //timer
 
-$(function() {
+	this.startNewGame = function(isStandard) {
+		if (isStandard != undefined) standard = isStandard;
+		if (standard) timer = 5;
+		else timer = 60;
+
+		$('#instructions').hide();
+		$('#conclusion').hide();
+		$('#game').show();
+		randomize();
+		$('#score').text('Score: ' + score);
+		$('#countdown').text('Time Left: ' + timer);
+		countdown = setInterval(function () {
+			if (--timer > 0) {
+				$('#countdown').text('Time Left: ' + timer);
+			} else {
+				clearInterval(countdown);
+				$('#game').hide();
+				$('#result').text('Your Score: ' + score);
+				var highScore = 0;
+				if (standard) {
+					highScore = localStorage.ShadesStandardHighScore;
+					if (score > highScore || !highScore) {
+						localStorage.ShadesStandardHighScore = score;
+						highScore = score;
+					}
+				} else {
+					highScore = localStorage.ShadesTimedHighScore;
+					if (score > highScore || !highScore) {
+						localStorage.ShadesTimedHighScore = score;
+						highScore = score;
+					}
+				}
+				$('#highScore').text('High Score: ' + highScore);
+				$('#conclusion').show();
+				resetGame();
+			}
+		}, 1000);
+	};
+
+	function randomize() {
+		var content = $('#blocks');
+		content.empty();
+		var numBlocks = Math.pow(size, 2);
+		var r = Math.floor(Math.random()*206), g = Math.floor(Math.random()*206), b = Math.floor(Math.random()*206);
+		for (var i=0; i<numBlocks; i++) {
+			content.append('<div id=' + i + ' style="margin: ' + initSize*0.05 + '%; background-color: rgb('+r+','+g+','+b+'); width: ' + initSize*0.9 + '%; height: ' + initSize*0.9 + '%;"></div>');
+		}
+		odd = Math.floor(Math.random()*numBlocks);
+		$('#' + odd).css('background-color', 'rgb('+(r+threshold)+','+(g+threshold)+','+(b+threshold)+')');
+		$('#blocks > div').click(function() {
+			if ($(this).attr('id') == odd) {
+				score += 10;
+				if (standard) {
+					inc++;
+					if (inc%2 == 0) timer++;
+					else timer += 2;
+					$('#countdown').text('Time Left: ' + timer);
+				}
+				$('#score').text('Score: ' + score);
+				computeNewThreshold();
+				computeNewSize();
+				randomize();
+			} else {
+				score -= 5;
+				$('#score').text('Score: ' + score);
+			}
+		});
+	}
+
+	//decrease threshold every 1 hit, 1 hit, 2, 2, 3, 3,....
+	function computeNewThreshold() {
+		thresCount++;
+		if (thresCount == thresInc) {
+			levelCounter++;
+			thresModder++;
+			thresCount = 0;
+			if (thresModder == 2) {
+				thresInc++;
+				thresModder = 0;
+			}
+			threshold = Math.ceil(250/(2.3*levelCounter + 5));
+		}
+	}
+
+	//increase 1 column and 1 row of blocks every 1 hit, 2 hits, 3, 4, ....
+	function computeNewSize() {
+		sizeCount++;
+		if (sizeCount == sizeInc) {
+			sizeCount = 0;
+			sizeInc++;
+			size++;
+			initSize = 100/size;
+		}
+	}
+
+	function resetGame() {
+		score = 0;
+		initSize = 50;
+		threshold = 50;
+		inc = 0;
+		levelCounter = 0;
+
+		thresCount = 0;
+		thresInc = 1;
+		thresModder = 0;
+
+		sizeCount = 0;
+		sizeInc = 1;
+		size = 2;
+	}
+
+	this.endGame = function () {
+		resetGame();
+		clearInterval(countdown);
+	};
+};
+
+$(function () {
+	game = new ShadesGame();
+
 	$('#standard').click(function() {
-		standard = true;
-		startNewGame();
+		game.startNewGame(true);
 	});
 
 	$('#timed').click(function() {
-		standard = false;
-		startNewGame();
+		game.startNewGame(false);
 	});
 
 	$('#highScores').click(function() {
@@ -30,7 +148,7 @@ $(function() {
 
 	$('#playAgain').click(function() {
 		$('#conclusion').hide();
-		startNewGame();
+		game.startNewGame();
 	});
 
 	$('#menuBtn').click(function() {
@@ -43,120 +161,3 @@ $(function() {
 		$('#instructions').show();
 	});
 });
-
-function startNewGame() {	
-	if (standard) timer = 3;
-	else timer = 60;
-
-	$('#instructions').hide();
-	$('#conclusion').hide();
-	$('#game').show();
-	randomize();	
-	$('#score').text('Score: ' + score);
-	$('#countdown').text('Time Left: ' + timer);
-	countdown = setInterval(function () {
-		if (--timer > 0) {
-			$('#countdown').text('Time Left: ' + timer);
-		} else {
-			clearInterval(countdown);
-			$('#game').hide();
-			$('#result').text('Your Score: ' + score);
-			var highScore = 0;
-			if (standard) {
-				highScore = localStorage.ShadesStandardHighScore;
-				if (score > highScore || !highScore) {
-					localStorage.ShadesStandardHighScore = score;
-					highScore = score;
-				}
-			} else {
-				highScore = localStorage.ShadesTimedHighScore;
-				if (score > highScore || !highScore) {
-					localStorage.ShadesTimedHighScore = score;
-					highScore = score;
-				}
-			}
-			$('#highScore').text('High Score: ' + highScore);
-			$('#conclusion').show();
-			resetGame();
-		}
-	}, 1000);
-}
-
-function randomize() {
-	var content = $('#blocks');
-	content.empty();
-	var numBlocks = Math.pow(size, 2);
-	var r = Math.floor(Math.random()*206), g = Math.floor(Math.random()*206), b = Math.floor(Math.random()*206);
-	for (var i=0; i<numBlocks; i++) {
-		content.append('<div id=' + i + ' style="margin: ' + initSize*0.05 + '%; background-color: rgb('+r+','+g+','+b+'); width: ' + initSize*0.9 + '%; height: ' + initSize*0.9 + '%;"></div>');
-	}	
-	odd = Math.floor(Math.random()*numBlocks);
-	$('#' + odd).css('background-color', 'rgb('+(r+threshold)+','+(g+threshold)+','+(b+threshold)+')');
-	$('#blocks > div').click(function() {
-		if ($(this).attr('id') == odd) {
-			score += 10;
-			if (standard) {
-				inc++;
-				if (inc == 3) {
-					timer += 5;
-					inc = 0;
-					$('#countdown').text('Time Left: ' + timer);
-				}
-			}
-			$('#score').text('Score: ' + score);
-			computeNewThreshold();
-			computeNewSize();
-			randomize();
-		} else {
-			score -= 5;
-			$('#score').text('Score: ' + score);
-		}
-	});
-}
-
-//decrease threshold every 1 hit, 1 hit, 2, 2, 3, 3,....
-function computeNewThreshold() {
-	thresCount++;
-	if (thresCount == thresInc) {
-		levelCounter++;
-		thresModder++; 
-		thresCount = 0;
-		if (thresModder == 2) {
-			thresInc++;	
-			thresModder = 0;
-		}
-		threshold = Math.ceil(250/(2.3*levelCounter + 5));	
-	}
-}
-
-//increase 1 column and 1 row of blocks every 1 hit, 2 hits, 3, 4, ....
-function computeNewSize() {
-	sizeCount++;
-	if (sizeCount == sizeInc) {
-		sizeCount = 0;
-		sizeInc++;
-		size++;
-		initSize = 100/size;
-	}
-}
-
-function resetGame() {
-	score = 0;
-	initSize = 50;
-	threshold = 50;
-	inc = 0;
-	levelCounter = 0;
-
-	thresCount = 0;
-	thresInc = 1;
-	thresModder = 0;
-
-	sizeCount = 0;
-	sizeInc = 1;
-	size = 2;
-}
-
-function endShadesGame() {
-	resetGame();
-	clearInterval(countdown);
-}
